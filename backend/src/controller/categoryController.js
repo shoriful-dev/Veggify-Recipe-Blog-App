78d9c8cc81f6308b7388f153edffc35525b859f2
shoriful-dev <cmshohelrana50@gmail.com>
2025-08-24 01:15:09 +0600

    add category controller

--- backend/src/controller/categoryController.js
+++ backend/src/controller/categoryController.js
@@ -1,21 +1,21 @@
-// //fetch items based on category
+//fetch items based on category
 
-// const Category = require('../model/categoryModel');
-// const Item = require('../model/itemModel');
+const Category = require('../model/categoryModel');
+const Item = require('../model/itemModel');
 
-// const getCategory = async (req, res) => {
-//   const { category } = req.params;
-//   try {
-//     const categoryData = await Category.findOne({ name: category });
-//     if (!categoryData)
-//       return res.status(404).json({ message: 'Category not found' });
-//     const items = await Item.find({ menuId: categoryData.menuId });
-//     res.json(items);
-//   } catch (error) {
-//     res.status(500).json({ message: 'Server error' });
-//   }
-// };
+const getCategory = async (req, res) => {
+  const { category } = req.params;
+  try {
+    const categoryData = await Category.findOne({ name: category });
+    if (!categoryData)
+      return res.status(404).json({ message: 'Category not found' });
+    const items = await Item.find({ menuId: categoryData.menuId });
+    res.json(items);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' });
+  }
+};
 
-// module.exports = {
-//   getCategory,
-// };
+module.exports = {
+  getCategory,
+};
