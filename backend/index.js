2573cc9c17b83334951ecb6e12606a1128063705
shoriful-dev <cmshohelrana50@gmail.com>
2025-08-25 04:21:10 +0600

    All Done

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const app = express()
+const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -12,12 +12,11 @@ app.use(cors());
 
 async function main() {
   await mongoose.connect(process.env.MONGO_URL);
-  app.get('/', (req, res) => {
-    res.send('Veggify Recipe App Server is running');
-  });
 }
 
-main().then(() => console.log('MongoDB Connected Successfully')).catch(err => console.log(err));
+main()
+  .then(() => console.log('MongoDB Connected Successfully'))
+  .catch(err => console.log(err));
 
 // routes
 const ItemRoutes = require('./src/routes/itemRoute');
@@ -26,6 +25,10 @@ const CategoryRoutes = require('./src/routes/categoryRoute');
 app.use('/api', ItemRoutes);
 app.use('/api', CategoryRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Veggify Recipe App Server is running');
+});
+
 app.listen(port, () => {
-  console.log(`This port is running on ${port} port`)
-})
+  console.log(`This port is running on ${port} port`);
+});
